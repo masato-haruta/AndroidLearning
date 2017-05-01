2cb4a488f61baf57f8ab2a45875183dcac012cfc
Masato Haruta <haruta@328w.co.jp>
2017-05-01 19:06:57 +0900

    削除機能を追加した

--- app/src/main/java/com/example/haruta/myapplication/ListViewActivity.java
+++ app/src/main/java/com/example/haruta/myapplication/ListViewActivity.java
@@ -1,10 +1,14 @@
 package com.example.haruta.myapplication;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -19,7 +23,7 @@ public class ListViewActivity extends AppCompatActivity {
 
     private ListView mListView;
 
-    private ArrayAdapter<String> mAdapter;
+    private MyAdapter mAdapter;
 
     @BindView(R.id.listview_button_add)
     Button mAddListButton;
@@ -33,9 +37,9 @@ public class ListViewActivity extends AppCompatActivity {
 
         mListView = (ListView) findViewById(R.id.my_listView);
 
-        mAdapter = new ArrayAdapter<String>(
+        mAdapter = new MyAdapter(
                 this,
-                android.R.layout.simple_list_item_1
+                android.R.layout.activity_list_item
         );
 
         mListView.setAdapter(mAdapter);
@@ -46,17 +50,63 @@ public class ListViewActivity extends AppCompatActivity {
         mAdapter.add(getDateText());
     }
 
-    @OnItemLongClick(R.id.my_listView)
-    public boolean onItemLongClick(int position) {
-        String selectedItem = (String) mListView.getItemAtPosition(position);
-        mAdapter.remove(selectedItem);
-        return false;
-    }
-
     // 現在時刻を返す
     private String getDateText() {
         Date date = new Date();
         SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
         return simpleDateFormat.format(date);
+    }
+
+    // カスタムAdapter
+    static class MyAdapter extends ArrayAdapter<String> {
+
+        private Context mContext;
+
+        private String mText;
+
+        public MyAdapter(Context context, int resourceId) {
+            super(context, resourceId);
+            mContext = context;
+        }
+
+        @Override
+        public View getView(final int position, View convertView, final ViewGroup parent) {
+            final ViewHolder holder;
+            mText = getItem(position);
+
+            // convertViewは使い回しされている可能性があるのでnullの時だけ新しく作る
+            if (null == convertView) {
+                convertView = View.inflate(mContext, R.layout.activity_listview_row, null);
+                holder = new ViewHolder(convertView);
+                convertView.setTag(holder);
+            } else {
+                holder = (ViewHolder) convertView.getTag();
+            }
+
+            holder.mBody.setText(mText);
+
+            holder.mDeleteButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    ListView listView = (ListView) parent;
+                    String selectedItem = (String) listView.getItemAtPosition(position);
+                    remove(selectedItem);
+                }
+            });
+
+            return convertView;
+        }
+
+        static class ViewHolder {
+            @BindView(R.id.listview_text_body)
+            TextView mBody;
+
+            @BindView(R.id.listview_button_delete)
+            Button mDeleteButton;
+
+            ViewHolder(View view) {
+                ButterKnife.bind(this, view);
+            }
+        }
     }
 }
