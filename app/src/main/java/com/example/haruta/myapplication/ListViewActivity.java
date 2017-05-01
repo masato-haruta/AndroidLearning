fe03879254ff3afd0262fcddd1d084afdb1b193a
Masato Haruta <haruta@328w.co.jp>
2017-05-01 16:17:46 +0900

    リストに動的に内容を追加できるようにした

--- app/src/main/java/com/example/haruta/myapplication/ListViewActivity.java
+++ app/src/main/java/com/example/haruta/myapplication/ListViewActivity.java
@@ -3,36 +3,52 @@ package com.example.haruta.myapplication;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 
 public class ListViewActivity extends AppCompatActivity {
 
     private ListView mListView;
 
+    private ArrayAdapter<String> mAdapter;
+
+    @BindView(R.id.listview_button_add)
+    Button mAddListButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listview);
 
-        String[] members = {
-                "osumi",
-                "kamezawa",
-                "haruta",
-                "kubo",
-                "kakimoto",
-                "fumy",
-                "leon"
-        };
+        ButterKnife.bind(this);
 
         mListView = (ListView) findViewById(R.id.my_listView);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+        mAdapter = new ArrayAdapter<String>(
                 this,
-                android.R.layout.simple_list_item_1,
-                members
+                android.R.layout.simple_list_item_1
         );
 
-        mListView.setAdapter(adapter);
+        mListView.setAdapter(mAdapter);
+    }
+
+    @OnClick(R.id.listview_button_add)
+    public void addList() {
+        mAdapter.add(getDateText());
+    }
+
+    // 現在時刻を返す
+    private String getDateText() {
+        Date date = new Date();
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        return simpleDateFormat.format(date);
     }
 }
